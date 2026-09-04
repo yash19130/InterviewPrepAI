4f282dcf1b6ed5295d378bfbcba68e310548aaad
Yash Tanwar <yashtanwar@Yashs-MacBook-Air.local>
2026-09-04 20:21:32 +0530

    Fix jwks-rsa and jose ESM interop by adding them to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["firebase-admin"],
+  serverExternalPackages: ["firebase-admin", "jwks-rsa", "jose"],
 };
 
 export default nextConfig;
